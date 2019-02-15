bf686ed4dff601ef7539b4f5a4949794f426ee07
agra <agramatik@spotoption.com>
2019-02-15 18:37:31 +0200

    scroll categories

--- src/components/jokeHolder/jokeHolder.js
+++ src/components/jokeHolder/jokeHolder.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import Loader from '../loader'
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
